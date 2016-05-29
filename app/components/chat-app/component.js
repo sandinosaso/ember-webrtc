63d81db113b97b5565036d391897d6a2c2a59461
sandinosaso <sandinosaso@gmail.com>
2016-05-28 23:03:03 -0300

    Restructura del projecto en folders: client (Ember app), server (SignalServer, NodePushNotificationServer). Deteccion de sonido.

--- app/components/chat-app/component.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-  currentUserService: Ember.inject.service('current-user'),
-  nameIsSet: Ember.computed('currentUserService.user', function() {
-    return this.get('currentUserService').hasUser();
-  })
-});
--- server/node-pusher-server/app.js
+++ server/node-pusher-server/app.js
@@ -32,6 +32,12 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 app.post("/messages", function(req, res){
   var data = req.body;
