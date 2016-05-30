e2d9d0ec9d7d9ebffc7ecb9a5109df6ebca050af
sandinosaso <sandinosaso@gmail.com>
2016-05-30 00:24:21 -0300

    Comparto archivos pero pierdo video

--- client/app/services/video-chat.js
+++ client/app/services/video-chat.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 import SocketIo1xConnection from '../utils/SocketIo1xConnection';
 
+const {
+  get
+} = Ember;
+
 export default Ember.Service.extend(Ember.Evented, {
 
     sessionManager: null,
@@ -15,6 +19,7 @@ export default Ember.Service.extend(Ember.Evented, {
             nick: 'xander1',
             localVideoEl: null,
             autoRequestMedia: false,
+            media: { audio: true, video: true },
             connection: new SocketIo1xConnection({
                 url: 'http://localhost:8888',
                 port: '8888',
@@ -32,9 +37,15 @@ export default Ember.Service.extend(Ember.Evented, {
         this.on('localMediaStarted', this.connectToRoom.bind(this));
 
         window.videochat = this;
-        this.get('store').pushPayload({people: [ { id: 'xander1', name: 'Xander1' } ] });
-        this.get('store').pushPayload({people: [ { id: 'xander2', name: 'Xander2' } ] });
-        this.get('store').pushPayload({people: [ { id: 'xander3', name: 'Xander3' } ] });
+        this.get('store').push({
+          data: {
+            id: '1',
+            type: 'person',
+            attributes: {
+              name: 'xander1'
+            }
+          }
+        });
     },
 
     joinRoom(media) {
@@ -45,6 +56,7 @@ export default Ember.Service.extend(Ember.Evented, {
             } else {
                 this.set('localMedia', stream);
                 this.trigger('localMediaStarted', stream);
+                this.trigger('createdPeer', stream);
             }
         });
     },
@@ -88,9 +100,221 @@ export default Ember.Service.extend(Ember.Evented, {
         this.get('sessionManager').joinRoom('emberjs');
     },
 
-    createdPeer(peer) {
+    // createdPeer(peer) {
+    //     // receiving an incoming filetransfer
+    //     peer.on('fileTransfer', function (metadata, receiver) {
+    //         console.log('incoming filetransfer', metadata.name, metadata);
+    //         receiver.on('progress', function (bytesReceived) {
+    //             console.log('receive progress', bytesReceived, 'out of', metadata.size);
+    //         });
+    //         // get notified when file is done
+    //         receiver.on('receivedFile', function (file, metadata) {
+    //             console.log('received file', metadata.name, metadata.size);
 
+    //             // close the channel
+    //             receiver.channel.close();
+    //         });
+    //     });
+    // },
+
+    createdPeer(peer) {
+      console.log('createdPeer', peer);
+      let remotes = document.getElementById('remotes');
+      if (!remotes) return;
+      let container = document.createElement('div');
+      container.className = 'peerContainer';
+      container.id = 'container_' + peer.id;
+
+      // show the peer id
+      // var peername = document.createElement('div');
+      // peername.className = 'peerName';
+      // peername.appendChild(document.createTextNode('Peer: ' + peer.id));
+      // container.appendChild(peername);
+
+      // show a list of files received / sending
+      let filelist = document.createElement('ul');
+      filelist.className = 'fileList';
+      container.appendChild(filelist);
+
+      // show a file select form
+      let fileinput = document.getElementById('file_to_share');
+
+      // send a file
+      fileinput.addEventListener('change', function() {
+          fileinput.disabled = true;
+
+          var file = fileinput.files[0];
+          var sender = peer.sendFile(file);
+
+          // create a file item
+          var item = document.createElement('li');
+          item.className = 'sending';
+
+          // make a label
+          var span = document.createElement('span');
+          span.className = 'filename';
+          span.appendChild(document.createTextNode(file.name));
+          item.appendChild(span);
+
+          span = document.createElement('span');
+          span.appendChild(document.createTextNode(file.size + ' bytes'));
+          item.appendChild(span);
+
+          // create a progress element
+          var sendProgress = document.createElement('progress');
+          sendProgress.max = file.size;
+          item.appendChild(sendProgress);
+
+          // hook up send progress
+          sender.on('progress', function (bytesSent) {
+              sendProgress.value = bytesSent;
+          });
+          // sending done
+          sender.on('sentFile', function () {
+              item.appendChild(document.createTextNode('sent'));
+
+              // we allow only one filetransfer at a time
+              fileinput.removeAttribute('disabled');
+          });
+          // receiver has actually received the file
+          sender.on('complete', function () {
+              // safe to disconnect now
+          });
+          filelist.appendChild(item);
+
+      }, false);
+
+      fileinput.disabled = 'disabled';
+
+      // show the ice connection state
+      if (peer && peer.pc) {
+        let connstate = document.createElement('div');
+        connstate.className = 'connectionstate';
+        container.appendChild(connstate);
+        peer.pc.on('iceConnectionStateChange', function (event) {
+            let state = peer.pc.iceConnectionState;
+            console.log('state', state);
+            container.className = 'peerContainer p2p' + state.substr(0, 1).toUpperCase() + state.substr(1);
+            switch (state) {
+            case 'checking':
+                connstate.innerText = 'Connecting to peer...';
+                break;
+            case 'connected':
+            case 'completed': // on caller side
+                connstate.innerText = 'Connection established.';
+                // enable file sending on connnect
+                fileinput.removeAttribute('disabled');
+                break;
+            case 'disconnected':
+                connstate.innerText = 'Disconnected.';
+                break;
+            case 'failed':
+                // not handled here
+                break;
+            case 'closed':
+                connstate.innerText = 'Connection closed.';
+
+                  // disable file sending
+                  fileinput.disabled = 'disabled';
+                  // FIXME: remove container, but when?
+                  break;
+              }
+        });
+      }
+      remotes.appendChild(container);
+
+      // receiving an incoming filetransfer
+      peer.on('fileTransfer', function (metadata, receiver) {
+          console.log('incoming filetransfer', metadata);
+          var item = document.createElement('li');
+          item.className = 'receiving';
+
+          // make a label
+          var span = document.createElement('span');
+          span.className = 'filename';
+          span.appendChild(document.createTextNode(metadata.name));
+          item.appendChild(span);
+
+          span = document.createElement('span');
+          span.appendChild(document.createTextNode(metadata.size + ' bytes'));
+          item.appendChild(span);
+
+          // create a progress element
+          var receiveProgress = document.createElement('progress');
+          receiveProgress.max = metadata.size;
+          item.appendChild(receiveProgress);
+
+          // hook up receive progress
+          receiver.on('progress', function (bytesReceived) {
+              receiveProgress.value = bytesReceived;
+          });
+          // get notified when file is done
+          receiver.on('receivedFile', function (file, metadata) {
+              console.log('received file', metadata.name, metadata.size);
+              var href = document.createElement('a');
+              href.href = URL.createObjectURL(file);
+              href.download = metadata.name;
+              href.appendChild(document.createTextNode('download'));
+              item.appendChild(href);
+
+              // close the channel
+              receiver.channel.close();
+          });
+          filelist.appendChild(item);
+      });
     },
+
+    // sendFile(peer) {
+    //   // show a list of files received / sending
+    //   var filelist = document.createElement('ul');
+    //   filelist.className = 'fileList';
+    //   document.appendChild(filelist);
+
+    //   // show a file select form
+    //   var fileinput = document.createElement('input');
+    //   fileinput.type = 'file';
+
+    //   fileinput.addEventListener('change', function() {
+    //       fileinput.disabled = true;
+
+    //       var item = document.createElement('li');
+    //       // make a label
+    //       var span = document.createElement('span');
+    //       span.className = 'filename';
+    //       span.appendChild(document.createTextNode(file.name));
+    //       item.appendChild(span);
+
+    //       span = document.createElement('span');
+    //       span.appendChild(document.createTextNode(file.size + ' bytes'));
+    //       item.appendChild(span);
+
+    //       // create a progress element
+    //       var sendProgress = document.createElement('progress');
+    //       sendProgress.max = file.size;
+    //       item.appendChild(sendProgress);
+
+    //       var file = fileinput.files[0];
+    //       var sender = peer.sendFile(file);
+
+    //       // hook up send progress
+    //       sender.on('progress', function (bytesSent) {
+    //           sendProgress.value = bytesSent;
+    //       });
+    //       // sending done
+    //       sender.on('sentFile', function () {
+    //           item.appendChild(document.createTextNode('sent'));
+
+    //           // we allow only one filetransfer at a time
+    //           fileinput.removeAttribute('disabled');
+    //       });
+    //       // receiver has actually received the file
+    //       sender.on('complete', function () {
+    //           // safe to disconnect now
+    //       });
+    //       filelist.appendChild(item);
+
+    //   });
+    // },
 
     videoAdded(peer) {
         this.get('store').find('person', peer.nick).then((person) => {
