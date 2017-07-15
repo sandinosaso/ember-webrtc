f71aa1b7043116f0e2f8f069a8be25a03d779157
Sandino <sandinosaso@gmail.com>
2017-07-14 22:21:12 -0300

    Update server.js

--- server/signalmaster/server.js
+++ server/signalmaster/server.js
@@ -28,8 +28,8 @@ if (config.uid) process.setuid(config.uid);
 
 var httpUrl;
 if (config.server.secure) {
-    httpUrl = "https://localhost:" + port;
+    httpUrl = location.origin.replace(/^https/, 'https') + port;
 } else {
-    httpUrl = "http://localhost:" + port;
+    httpUrl = location.origin.replace(/^https/, 'http') + port;
 }
 console.log(yetify.logo() + ' -- signal master is running at: ' + httpUrl);
